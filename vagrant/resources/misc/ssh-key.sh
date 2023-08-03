e1e243750df1b9504fbf1f2901d80e7d1c8dc899
aj-data <70345165+aj-data@users.noreply.github.com>
2023-08-03 13:37:02 -0400

    update

--- vagrant/resources/misc/ssh-key.sh
+++ vagrant/resources/misc/ssh-key.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 function SSHKey {
-    ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+    ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
     chmod 0600 ~/.ssh/authorized_keys
+    cp -f /vagrant/resources/ssh/config ~/.ssh
 }
 
 SSHKey
