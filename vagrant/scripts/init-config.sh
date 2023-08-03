41dff55232cb4e4a0c0fffd88423bcc4f5deed6d
aj-data <70345165+aj-data@users.noreply.github.com>
2023-08-03 16:04:20 -0400

    update

--- vagrant/scripts/init-config.sh
+++ vagrant/scripts/init-config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash  
   
-function stopService {  
-    #echo "Stopping unattended-upgrades service..."  
-    echo "Deteniendo el servicio unattended-upgrades..."
-    sudo systemctl stop unattended-upgrades  
-}  
+#function stopService {  
+#    #echo "Stopping unattended-upgrades service..."  
+#    echo "Deteniendo el servicio unattended-upgrades..."
+#    sudo systemctl stop unattended-upgrades  
+#}  
   
-function removeService {  
-    #echo "Removing unattended-upgrades..."  
-    echo "Eliminando unattended-upgrades..."
-    sudo apt-get purge -y unattended-upgrades  
-}
+#function removeService {  
+#    #echo "Removing unattended-upgrades..."  
+#    echo "Eliminando unattended-upgrades..."
+#    sudo apt-get purge -y unattended-upgrades  
+#}
 
 function createTempFolder {
     echo "Creando carpeta temporal..."
