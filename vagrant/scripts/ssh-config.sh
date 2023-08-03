47c82dfebd872f1b650178dd9f0a5d9270a6d281
aj-data <70345165+aj-data@users.noreply.github.com>
2023-08-03 10:42:23 -0400

    update

--- vagrant/scripts/ssh-config.sh
+++ vagrant/scripts/ssh-config.sh
@@ -4,16 +4,14 @@ function installPackages {
     #echo "Installing necessary packages..."
 	echo "Instalando los paquetes necesarios..."   
     sudo apt-get install -y openssh-server openssh-client  
-} 
+}
 
 function createSSHKey {
 	#echo "generating ssh key"
     echo "Generando clave ssh"
-	ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
-	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-	#cp -f $RES_SSH_CONFIG ~/.ssh
-    chmod 0600 ~/.ssh/authorized_keys
-}
+    cp -f /vagrant/resources/misc/ssh-key.sh /etc/profile.d/ssh-key.sh
+	. /etc/profile.d/ssh-key.sh
+} 
 
 # Call the functions
 installPackages
