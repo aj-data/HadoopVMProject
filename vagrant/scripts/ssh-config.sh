274f0613c1609830f3dce6a5fd6c80dff9b77c9f
aj-data <70345165+aj-data@users.noreply.github.com>
2023-08-06 07:59:27 -0400

    update

--- vagrant/scripts/ssh-config.sh
+++ vagrant/scripts/ssh-config.sh
@@ -11,12 +11,20 @@ function overwriteSSHCopyId {
 	cp -f $RES_SSH_COPYID_MODIFIED /usr/bin/ssh-copy-id
 }
 
+#function createSSHKey {
+#	#echo "generating ssh key"
+#    echo "Generando clave ssh"
+#	ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
+#	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+#	cp -f $RES_SSH_CONFIG ~/.ssh
+#}
+
 function createSSHKey {
 	#echo "generating ssh key"
     echo "Generando clave ssh"
-	ssh-keygen -t rsa -P "" -f /home/vagrant/.ssh/id_rsa
+	ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -C vagrant -q -P ""
 	cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-	#cp -f $RES_SSH_CONFIG ~/.ssh
+	chown -R vagrant:vagrant /home/vagrant/.ssh
 }
 
 # Call the functions
