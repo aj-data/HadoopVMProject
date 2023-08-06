98df04096b4a79ab98495b54d951b0c377199e5a
aj-data <70345165+aj-data@users.noreply.github.com>
2023-08-06 11:46:42 -0400

    update

--- vagrant/scripts/setup-metastore.sh
+++ vagrant/scripts/setup-metastore.sh
@@ -1,37 +1,38 @@
 #!/bin/bash  
-  
-HADOOP_HOME=/usr/local/hadoop  
-HIVE_HOME=/usr/local/hive  
-  
+
+HADOOP_HOME=/usr/local/hadoop
+HIVE_HOME=/usr/local/hive
+
 function setupHiveEnv {  
     #echo "Setting up Hive environment variables..."
-    echo "Configurando variables de entorno de Hive..."  
-    echo 'export HADOOP_HOME=/usr/local/hadoop' >> $HIVE_HOME/conf/hive-env.sh  
-}  
-  
-function installMySQL {  
+    echo "Configurando variables de entorno de Hive..."
+    cp $HIVE_HOME/conf/hive-env.sh.template $HIVE_HOME/conf/hive-env.sh
+    echo 'export HADOOP_HOME=/usr/local/hadoop' >> $HIVE_HOME/conf/hive-env.sh
+}
+
+function installMySQL {
     #echo "Installing MySQL..."
-    echo "Instalando MySQL..."  
-    sudo apt-get install -y mysql-server  
-}  
-  
-function setupMetastoreDB {  
-    #echo "Setting up Hive metastore database..." 
-    echo "Configurando base de datos de metastore de Hive..." 
-    sudo mysql -u root -p -e "CREATE DATABASE metastore_db; CREATE USER 'hiveuser'@'localhost' IDENTIFIED BY 'hivepassword'; GRANT ALL PRIVILEGES ON metastore_db.* TO 'hiveuser'@'localhost'; FLUSH PRIVILEGES;"  
-}  
-  
-function setupHiveSite {  
+    echo "Instalando MySQL..."
+    sudo apt-get install -y mysql-server
+}
+
+function setupMetastoreDB {
+    #echo "Setting up Hive metastore database..."
+    echo "Configurando base de datos de metastore de Hive..."
+    sudo mysql -u root -p -e "CREATE DATABASE metastore_db; CREATE USER 'hiveuser'@'localhost' IDENTIFIED BY 'hivepassword'; GRANT ALL PRIVILEGES ON metastore_db.* TO 'hiveuser'@'localhost'; FLUSH PRIVILEGES;" 
+}
+
+function setupHiveSite {
     #echo "Setting up hive-site.xml..."
-    echo "Configurando hive-site.xml..."  
-    cp $HIVE_HOME/conf/hive-default.xml.template $HIVE_HOME/conf/hive-site.xml  
+    echo "Configurando hive-site.xml..."
+    cp $HIVE_HOME/conf/hive-default.xml.template $HIVE_HOME/conf/hive-site.xml
     # Add your properties to hive-site.xml here
-    properties=(  
+    properties=(
     "javax.jdo.option.ConnectionURL jdbc:mysql://localhost/metastore_db?createDatabaseIfNotExist=true"  
     "javax.jdo.option.ConnectionDriverName com.mysql.cj.jdbc.Driver"  
     "javax.jdo.option.ConnectionUserName hiveuser"  
     "javax.jdo.option.ConnectionPassword hivepassword"   
-    )  
+    )
     
     for i in "${properties[@]}"; do  
         name=$(echo $i | cut -d' ' -f1)  
@@ -42,9 +43,9 @@ function setupHiveSite {
             sed -i "/<\/configuration>/i <property>\n<name>$name</name>\n<value>$value</value>\n</property>" $HIVE_HOME/conf/hive-site.xml  
         fi  
     done   
-}   
+}
 
-function fixWarnings {  
+function fixWarnings {
     #echo "Fixing Hive warning..."
     echo "Corrigiendo advertencia de Hive..."  
     #sudo rm /usr/local/hive/lib/log4j-slf4j-impl-2.17.1.jar
@@ -56,36 +57,36 @@ function fixWarnings {
     else
     sed -i "/<configuration>/a \\\n    <property>\\\n        <name>$PROP_NAME</name>\\\n        <value>$PROP_VALUE</value>\\\n    </property>" "$HIVE_SITE_XML"
     fi
-} 
+}
 
-function installMySQLJavaConnector {  
+function installMySQLJavaConnector {
     #echo "Installing MySQL Java Connector..."
     echo "Instalando MySQL Java Connector..."  
     wget -P /tmp/temp https://downloads.mysql.com/archives/get/p/3/file/mysql-connector-java-8.0.32.tar.gz  
     tar -xzvf /tmp/temp/mysql-connector-java-8.0.32.tar.gz -C /tmp/temp --remove-files  
     cp /tmp/temp/mysql-connector-java-8.0.32/mysql-connector-java-8.0.32.jar $HIVE_HOME/lib/  
-}  
-  
-function initMetastore {  
+}
+
+function initMetastore {
     #echo "Initializing Hive metastore..."
-    echo "Inicializando metastore de Hive..."  
-    schematool -initSchema -dbType mysql  
-}  
-  
-function setupHiveLocation {  
+    echo "Inicializando metastore de Hive..."
+    schematool -initSchema -dbType mysql
+}
+
+function setupHiveLocation {
     #echo "Setting up default Hive location..."
-    echo "Configurando ubicación predeterminada de Hive..."  
-    hdfs dfs -mkdir -p /user/hive/warehouse  
-    hdfs dfs -chmod g+w /tmp  
-    hdfs dfs -chmod g+w /user/hive/warehouse  
-}  
-  
-# Call the functions  
-setupHiveEnv  
-installMySQL  
-setupMetastoreDB  
+    echo "Configurando ubicación predeterminada de Hive..."
+    hdfs dfs -mkdir -p /user/hive/warehouse
+    hdfs dfs -chmod g+w /tmp
+    hdfs dfs -chmod g+w /user/hive/warehouse
+}
+
+# Call the functions
+setupHiveEnv
+installMySQL
+setupMetastoreDB
 setupHiveSite
-fixWarnings  
-installMySQLJavaConnector  
-initMetastore  
+fixWarnings
+installMySQLJavaConnector
+initMetastore
 setupHiveLocation
