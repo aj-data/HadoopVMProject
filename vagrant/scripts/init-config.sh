e0163c44c6b66482eb244a78adfe08f99e334c49
aj-data <70345165+aj-data@users.noreply.github.com>
2023-08-24 10:37:32 -0400

    update

--- vagrant/scripts/init-config.sh
+++ vagrant/scripts/init-config.sh
@@ -29,9 +29,9 @@ function createScriptsFolder {
 function setupHosts {
 	#echo "modifying /etc/hosts file"
     echo "Modificando archivo /etc/hosts..."
-        echo "127.0.0.1 node1" >> /etc/nhosts
-	echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" >> /etc/nhosts
-	echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/nhosts
+    echo "127.0.0.1 node1" >> /etc/nhosts
+	echo "127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4" >> /etc/nhosts
+	echo "::1 localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/nhosts
 	cp /etc/nhosts /etc/hosts
 	rm -f /etc/nhosts
 }
